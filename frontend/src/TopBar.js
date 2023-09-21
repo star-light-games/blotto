df3eecc2d1523c6fa08aa69e0e7ed312b564da51
Adam Yedidia <adamyedidia@gmail.com>
2023-09-21 01:47:25 -0400

    How to play

--- frontend/src/TopBar.js
+++ frontend/src/TopBar.js
@@ -5,6 +5,7 @@ import { useContext } from 'react';
 import {DarkModeContext} from './DarkModeContext';
 import Switch from '@mui/material/Switch';
 import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
 
 function DarkModeToggle() {
     const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
@@ -18,22 +19,39 @@ function DarkModeToggle() {
   }
   
 
-const TopBar = () => {
+  const TopBar = () => {
     return (
       <AppBar position="static" style={{ backgroundColor: 'black' }}>
         <Toolbar>
-          <Link to="/" style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
-            <img src={logo} alt="BlottoBattler Logo" style={{ height: '50px', marginRight: '10px' }} />
-          </Link>
-          <Typography variant="h6" style={{ flexGrow: 1 }}>
-            BlottoBattler
-          </Typography>
-          <Box display="flex" alignItems="center">
-            <DarkModeToggle />
-          </Box>
+          <Grid container alignItems="center" justify="space-between">
+            
+            {/* Left: Logo */}
+            <Grid item xs={4} container alignItems="center">
+              <Link to="/" style={{ textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
+                <img src={logo} alt="BlottoBattler Logo" style={{ height: '50px', marginRight: '10px' }} />
+              </Link>
+            </Grid>
+            
+            {/* Center: How to play */}
+            <Grid item xs={4} container justifyContent="center">
+              <Link external to="https://docs.google.com/document/d/12pSa81NvMgi8WRoDZQO4FrHMudNGaGx1aVrUjOtKp6w/edit?usp=sharing">
+                <Typography variant='h5'>How to play</Typography>
+              </Link>
+            </Grid>
+            
+            {/* Right: Title and DarkModeToggle */}
+            <Grid item xs={4} container justifyContent="flex-end" alignItems="center">
+              <Typography variant="h6">
+                BlottoBattler
+              </Typography>
+              <Box display="flex" alignItems="center" ml={2}>
+                <DarkModeToggle />
+              </Box>
+            </Grid>
+
+          </Grid>
         </Toolbar>
       </AppBar>
     );
 }
-
 export default TopBar;
