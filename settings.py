787cc449ad0ce38e2599366dba3dcdd48e3c3dfd
Adam Yedidia <adamyedidia@gmail.com>
2023-09-29 23:06:23 -0400

    Add alembic

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
+import os
+
 LOCAL = False
 COMMON_DECK_USERNAME = 'COMMON_DECK'
 BOT_DECK_USERNAME = 'BOT_DECK'
+DATABASE_URL = "postgresql://bb:blotto@localhost:5432/bb"
 
-from local_settings import *
+if os.path.exists('local_settings.py'):
+    from local_settings import *
