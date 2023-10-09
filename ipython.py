2defc591c38d202e0032503b715e7afb25fdb802
Adam Yedidia <adamyedidia@gmail.com>
2023-10-09 17:43:24 -0400

    Add some stuff to ipython

--- ipython.py
+++ ipython.py
@@ -1,6 +1,31 @@
-import IPython
+from IPython import embed
 
 from redis_utils import *
 from utils import *
 
-IPython.embed()
+def main():
+    sess = SessionLocal()
+
+    gpt2_small = sess.query(Model).filter(Model.name == "gpt2-small").one_or_none()
+
+    # add objects that you want to use in the shell to this dictionary
+    user_ns = {
+        "sess": sess, 
+        "Resid": Resid,
+        "Model": Model,
+        "User": User,
+        "Direction": Direction,
+        "DirectionDescription": DirectionDescription,
+        "Prompt": Prompt,
+        "func": func,
+        "gpt2_small": gpt2_small,
+        "enc": enc,
+    }
+
+    embed(user_ns=user_ns)
+
+    sess.close()
+
+if __name__ == "__main__":
+    main()
+
